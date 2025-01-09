071ca9f1bdf7aaaac9e950728467c58a92ce0b2c
GuiMV <guilherme.moizinho17@gmail.com>
2025-01-09 11:46:29 -0300

    BEE 1010 - Cálculo Simples

---
+++ Bee 1010.py
@@ -0,0 +1,7 @@
+''' BEE 1010 - Cálculo Simples '''
+
+_, n1, v1 = map(float, input().split())
+_, n2, v2 = map(float, input().split())
+print(f"VALOR A PAGAR: R$ {(n1 * v1) + (n2 * v2):.2f}")
+
+# tempo: 0.000s
