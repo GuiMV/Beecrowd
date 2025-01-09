4c66b07ac4d7b00216691834909ea014e55bc301
GuiMV <guilherme.moizinho17@gmail.com>
2025-01-09 10:37:34 -0300

    1000 - 1005

---
+++ Bee 1004.py
@@ -0,0 +1,5 @@
+''' BEE 1004 - Produto Simples '''
+
+print("PROD =", int(input()) * int(input()))
+
+# tempo: 0.000s
---
+++ Bee 1005.py
@@ -0,0 +1,6 @@
+''' BEE 1005 - Média 1 '''
+
+m = (float(input()) * 3.5 + float(input()) * 7.5)/11
+print(f"MEDIA = {m:.5f}")
+
+# tempo: 0.000s
