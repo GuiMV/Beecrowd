78ae07aa02265c6bce80ce58ca83443918c356a7
GuiMV <guilherme.moizinho17@gmail.com>
2025-01-13 09:55:22 -0300

    BEE 1012 - Área

---
+++ Bee 1012.py
@@ -0,0 +1,10 @@
+''' BEE 1012 - Área '''
+
+A, B, C = map(float, input().split())
+print(f'''TRIANGULO: {(A * C)/2:.3f}
+CIRCULO: {3.14159 * (C**2):.3f}
+TRAPEZIO: {((A + B)*C)/2:.3f}
+QUADRADO: {B**2:.3f}
+RETANGULO: {A*B:.3f}''')
+
+# tempo: 0.000s
